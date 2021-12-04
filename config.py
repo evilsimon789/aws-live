369accc826de6e60dfa607642ad57ff3c3e8d822
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-04 18:16:43 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cbbckgynjra4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "simonchenyiheng-employee"
+customregion = "us-east-1"
