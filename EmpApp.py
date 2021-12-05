7e492e491343256d8c21bcb65e5e805ea4a78954
evilsimon789 <58799463+evilsimon789@users.noreply.github.com>
2021-12-05 16:30:18 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -34,22 +34,6 @@ def about():
 def getEmp():
     return render_template('GetEmp.html')
 
-@app.route("/fetchdata", methods=['POST'])
-def getEmp():
-    emp_id = request.form['emp_id']
-
-    cursor = db_conn.cursor()
-    read_sql = "SELECT * FROM employee WHERE emp_id = %s"
-    cursor.execute(read_sql, (emp_id))
-    result = cursor.fetchall()
-	
-    for row in result:
-        first_name = row["first_name"]
-	last_name = row["last_name"]
-   	location = row["location"]
-
-    return render_template('GetEmpOutput.html', id=emp_id, fname=first_name, lname=last_name, interest=0, location=location)
-
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
     emp_id = request.form['emp_id']
